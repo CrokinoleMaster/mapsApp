c9cfa653e6ce7986e22020500d4e1cec294a7520
huarui <huarui.wu@gmail.com>
2014-04-16 11:53:54 -0700

    add more maps

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module( 'myApp', ['ngRoute', 'google-maps'] );
 
-app.controller('MapController', function($scope){
+app.controller('MapController1', function($scope){
 
     $scope.map = {
         center: {
@@ -31,3 +31,73 @@ app.controller('MapController', function($scope){
 
     };
 })
+
+app.controller('MapController2', function($scope){
+
+    $scope.map = {
+        center: {
+            latitude: 37.77,
+            longitude: -122.42
+        },
+        zoom: 13,
+        markClick: false,
+        fit: true,
+        draggable: true,
+
+        showHeatmap: true,
+        showTraffic: true,
+        showBike: true,
+
+        heatLayerCallback: function(layer){
+            var mockHeatLayer = new MockHeatLayer(layer);
+        },
+        toggleHeat: function(){
+            $scope.map.showHeatmap = !$scope.map.showHeatmap;
+        },
+        toggleTraffic: function(){
+            $scope.map.showTraffic = !$scope.map.showTraffic;
+        },
+        toggleBike: function(){
+            $scope.map.showBike = !$scope.map.showBike;
+        }
+    };
+
+});
+
+app.controller('MapController3', function($scope){
+
+    $scope.map = {
+        center: {
+            latitude: 37.77,
+            longitude: -122.42
+        },
+        zoom: 13,
+        markClick: false,
+        fit: true,
+        draggable: true,
+
+        showHeatmap: false,
+        showTraffic: false,
+        showBike: false,
+
+        heatLayerCallback: function(layer){
+            var mockHeatLayer = new MockHeatLayer(layer);
+        },
+        toggleHeat: function(){
+            $scope.map.showHeatmap = !$scope.map.showHeatmap;
+            $scope.map.showTraffic = false;
+            $scope.map.showBike = false;
+        },
+        toggleTraffic: function(){
+            $scope.map.showTraffic = !$scope.map.showTraffic;
+            $scope.map.showHeatmap = false;
+            $scope.map.showBike = false;
+        },
+        toggleBike: function(){
+            $scope.map.showBike = !$scope.map.showBike;
+            $scope.map.showHeatmap = false;
+            $scope.map.showTraffic = false;
+        }
+    };
+
+});
