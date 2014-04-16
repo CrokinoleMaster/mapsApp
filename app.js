a8373cd8f28e9dce3efd558f911cf35bea9ee5ea
huarui <huarui.wu@gmail.com>
2014-04-16 12:17:05 -0700

    add routes

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 var app = angular.module( 'myApp', ['ngRoute', 'google-maps'] );
 
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: '/templates/home.html',
+            controller:  'HomeController'
+        })
+        .when('/sidebyside', {
+            templateUrl: '/templates/sidebyside.html',
+            controller: 'MapController1'
+        })
+        .when('/layered', {
+            templateUrl: '/templates/layered.html',
+            controller: 'MapController2'
+        })
+        .when('/singlelayered', {
+            templateUrl: '/templates/singlelayered.html',
+            controller: 'MapController3'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+app.controller('HomeController', function($scope){
+
+});
+
 app.controller('MapController1', function($scope){
 
     $scope.map = {
